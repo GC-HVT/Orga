cf92525d5572b89f79fd24c2c19ba0ddea43a4a5
GC-HVT <guillaume.collineau@herve-thermique.com>
2025-05-06 14:37:03 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -9,35 +9,42 @@ import {
 } from "../modules/moduleOrganigramme.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  initializeDiagram();
-
+  initializeDiagram("myDiagramDiv");
   chargerGroupes();
 
-  const loadBtn = document.getElementById("loadMembersBtn");
-  if (loadBtn) {
-    loadBtn.addEventListener("click", () => {
-      chargerMembres(ajouterMembresDansDiagramme);
-    });
-  }
+  document.getElementById("loadMembersBtn").addEventListener("click", () => {
+    chargerMembres(injecterMembresSidebar);
+  });
 
-  const groupSelect = document.getElementById("groupSelect");
-  if (groupSelect) {
-    groupSelect.addEventListener("change", () => {
-      chargerMembres(ajouterMembresDansDiagramme);
-    });
-  }
+  document.getElementById("groupSelect").addEventListener("change", () => {
+    chargerMembres(injecterMembresSidebar);
+  });
 
-  document.getElementById("addNodeBtn")?.addEventListener("click", addNode);
-  document.getElementById("addLinkBtn")?.addEventListener("click", addLink);
-  document.getElementById("resetBtn")?.addEventListener("click", resetDiagram);
-  document.getElementById("exportBtn")?.addEventListener("click", exportDiagram);
+  document.getElementById("addNodeBtn").addEventListener("click", () => addNode());
+  document.getElementById("addLinkBtn").addEventListener("click", () => addLink());
+  document.getElementById("resetBtn").addEventListener("click", () => resetDiagram());
+  document.getElementById("exportBtn").addEventListener("click", () => exportDiagram());
 });
 
-function ajouterMembresDansDiagramme(membres) {
-  const donnees = membres.map((membre, index) => ({
-    key: membre.id || index.toString(),
-    name: membre.displayName || "Sans nom"
-  }));
-  console.log("Ajout au diagramme :", donnees);
-  addMembersToDiagram(donnees);
+function injecterMembresSidebar(membres) {
+  const container = document.getElementById("membersList");
+  container.innerHTML = ""; // vider avant ajout
+
+  membres.forEach((membre, index) => {
+    const div = document.createElement("div");
+    div.className = "membre";
+    div.draggable = true;
+    div.dataset.id = membre.id || index.toString();
+    div.dataset.nom = membre.displayName || "Sans nom";
+    div.textContent = membre.displayName || "Sans nom";
+
+    div.addEventListener("dragstart", (e) => {
+      e.dataTransfer.setData("text/plain", JSON.stringify({
+        key: div.dataset.id,
+        name: div.dataset.nom
+      }));
+    });
+
+    container.appendChild(div);
+  });
 }
