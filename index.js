4e13ac2ac97d867aa0e25b36200a3abefaf67490
GC-HVT <guillaume.collineau@herve-thermique.com>
2025-05-06 11:27:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ document.addEventListener('DOMContentLoaded', async () => {
   // Initialiser l'organigramme GoJS
   initialiserOrganigramme();
 
+  import { chargerGroupes } from './membresModule.js';
+
+	chargerGroupes().then(groupes => {
+	  console.log('Groupes chargés :', groupes);
+	}).catch(error => {
+	  console.error('Erreur chargement groupes :', error);
+	});
+
+	
   // Charger les groupes dans le menu déroulant
   await chargerGroupes();
 
