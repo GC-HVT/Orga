6b060f6e3ea21b80f53857ffa9f40500c7c84ce0
GC-HVT <guillaume.collineau@herve-thermique.com>
2025-05-06 12:09:07 +0200

    Delete membresModule.js

--- membresModule.js
+++
@@ -1,9 +0,0 @@
-export function chargerGroupes() {
-  const contenu = document.getElementById('contenu');
-  contenu.textContent = "Les groupes sont en cours de chargement...";
-
-  // Simulation : ici tu pourras mettre ton vrai code (API, etc.)
-  setTimeout(() => {
-    contenu.textContent = "Groupes chargés : Groupe A, Groupe B, Groupe C.";
-  }, 1000);
-}
