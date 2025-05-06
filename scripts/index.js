4f2f87f4171809b8b97eab6b1f2114d40e513df3
GC-HVT <guillaume.collineau@herve-thermique.com>
2025-05-06 13:38:37 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,22 @@
 // index.js
-import { chargerGroupes, chargerMembres } from "../modules/moduleMembres.js";
-import { initDiagram, addMembersToDiagram, clearDiagram } from "../modules/moduleOrganigramme.js";
+import { chargerGroupes, chargerMembres } from "./modules/moduleMembres.js";
+import { initDiagram, addMembersToDiagram } from "./modules/moduleOrganigramme.js";
 
-// Initialiser le diagramme dans l'élément HTML
 document.addEventListener("DOMContentLoaded", () => {
-  // Initialisation du diagramme
   initDiagram("myDiagramDiv");
-
-  // Charger les groupes au démarrage
   chargerGroupes();
 
-  // Ajouter l'événement de chargement des membres lorsque le bouton est cliqué
-  const loadMembersBtn = document.getElementById("loadMembersBtn");
-  loadMembersBtn.addEventListener("click", chargerMembres);
-
-  // Initialiser les actions de création des nœuds dans le diagramme
-  document.getElementById("groupSelect").addEventListener("change", () => {
-    // Effacer le diagramme avant d'ajouter de nouveaux membres
-    clearDiagram();
+  const loadBtn = document.getElementById("loadMembersBtn");
+  loadBtn.addEventListener("click", () => {
+    chargerMembres(ajouterMembresDansDiagramme);
   });
 });
 
-// Fonction pour ajouter des membres à l'organigramme
 function ajouterMembresDansDiagramme(membres) {
-  console.log("Membres à ajouter au diagramme :", membres);
-  addMembersToDiagram(membres);
+  const donnees = membres.map((membre, index) => ({
+    key: membre.id || index.toString(),
+    name: membre.displayName || "Sans nom"
+  }));
+  console.log("Ajout au diagramme :", donnees);
+  addMembersToDiagram(donnees);
 }
